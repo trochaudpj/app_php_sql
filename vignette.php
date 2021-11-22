f5aad5905a8c961592b5f65be182f835eb4a20e3
trochaudpj <pjtggl@gmail.com>
2021-11-22 08:29:27 +0100

    corrections mineures

--- vignette.php
+++ vignette.php
@@ -1,10 +1,10 @@
 <?php echo "<div class='figure box ". $affichage." ";
 if (isset($row['name_categorie'])) echo  $row['name_categorie'] ;
 echo"'><img class='vignette' src='img/" . $row['img'] . "' width=200>";
-echo "<div><div><h2><a  href='product.php?id=" . $row['id'] . "'>" . $row['name'] ." ". "</a></h2></div><div><h2>" . $row['price'] . "€</h2></div></div><div><p>" . $row['description'] . "</p></div>";
+
+echo "<div><h3><a  href='product.php?id=" . $row['id'] . "'>" . $row['name'] ." ". "</a>" . $row['price'] . "€</h3></div><div><p>" . $row['description'] . "</p></div>";
 echo "";
 if (isset($row['name_categorie'])) echo '<em>#'. $row['name_categorie']."</em>" ;
-echo "</em>";
 echo "<form method='POST' action='traitement.php?action=addProd&id=" . $row['id'] . "'>"; 
 
 ?>
